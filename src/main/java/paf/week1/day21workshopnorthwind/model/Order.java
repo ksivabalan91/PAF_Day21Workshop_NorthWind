b828a4cb49a3012f6d857823c2a1f7c312c0b02d
K.Sivabalan <ksivabalan.91@gmail.com>
2023-03-10 09:19:52 +0800

    changed date from string to LocalDateTime format

--- src/main/java/paf/week1/day21workshopnorthwind/model/Order.java
+++ src/main/java/paf/week1/day21workshopnorthwind/model/Order.java
@@ -1,5 +1,7 @@
 package paf.week1.day21workshopnorthwind.model;
 
+import java.time.LocalDateTime;
+
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import jakarta.json.Json;
@@ -16,8 +18,8 @@ public class Order {
     private int id;
     private int employeeId;
     private int customerId;
-    private String orderDate;
-    private String shippedDate;
+    private LocalDateTime orderDate;
+    private LocalDateTime shippedDate;
     private int shipperId;
     private String shipName;
     private String shipAddress;
@@ -28,7 +30,7 @@ public class Order {
     private float shippingFee;
     private float taxes;
     private String paymentType;
-    private String paidDate;
+    private LocalDateTime paidDate;
     private double taxRate;
     private int statusId;
     @Override
@@ -51,10 +53,10 @@ public class Order {
             .add("tax_rate", taxRate)
             .add("status_id", statusId);
 
-        if(orderDate!=null){jsonBuilder.add("order_date", orderDate);}
-        if(shippedDate!=null){jsonBuilder.add("shipped_date", shippedDate);}
+        if(orderDate!=null){jsonBuilder.add("order_date", orderDate.toString());}
+        if(shippedDate!=null){jsonBuilder.add("shipped_date", shippedDate.toString());}
         if(paymentType!=null){jsonBuilder.add("payment_type", paymentType);}
-        if(paidDate!=null){jsonBuilder.add("paid_date", paidDate);}           
+        if(paidDate!=null){jsonBuilder.add("paid_date", paidDate.toString());}           
         JsonObject json = jsonBuilder.build();
 
         return json.toString();
@@ -65,8 +67,8 @@ public class Order {
         order.setId(rs.getInt("id"));
         order.setCustomerId(rs.getInt("customer_id"));
         order.setEmployeeId(rs.getInt("employee_id"));
-        order.setOrderDate(rs.getString("order_date"));
-        order.setShippedDate(rs.getString("shipped_date"));
+        order.setOrderDate((LocalDateTime)rs.getObject("order_date"));
+        order.setShippedDate((LocalDateTime)rs.getObject("shipped_date"));
         order.setShipperId(rs.getInt("shipper_id"));
         order.setShipName(rs.getString("ship_name"));
         order.setShipAddress(rs.getString("ship_address"));
@@ -77,7 +79,7 @@ public class Order {
         order.setShippingFee(rs.getFloat("shipping_fee"));
         order.setTaxes(rs.getFloat("taxes"));
         order.setPaymentType(rs.getString("payment_type"));
-        order.setPaidDate(rs.getString("paid_date"));
+        order.setPaidDate((LocalDateTime)rs.getObject("paid_date"));
         order.setTaxRate(rs.getDouble("tax_rate"));
         order.setStatusId(rs.getInt("status_id"));
 
@@ -103,10 +105,10 @@ public class Order {
             .add("tax_rate", taxRate)
             .add("status_id", statusId);
 
-        if(orderDate!=null){jsonBuilder.add("order_date", orderDate);}
-        if(shippedDate!=null){jsonBuilder.add("shipped_date", shippedDate);}
+        if(orderDate!=null){jsonBuilder.add("order_date", orderDate.toString());}
+        if(shippedDate!=null){jsonBuilder.add("shipped_date", shippedDate.toString());}
         if(paymentType!=null){jsonBuilder.add("payment_type", paymentType);}
-        if(paidDate!=null){jsonBuilder.add("paid_date", paidDate);}           
+        if(paidDate!=null){jsonBuilder.add("paid_date", paidDate.toString());}           
         
         JsonObject json = jsonBuilder.build();
         
